4eca9fc0601acea5f6e08a4acb5ff0064218dfc5
Ayden <krace2007@gmail.com>
2024-05-02 07:36:56 -0400

    uea

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
-window.addEventListener("load", function () {
-  const player = {
-    health: 100,
+let winner = "NULL";
+let IntervalId = "NotApplied";
+
+const player = {
+  maxHealth: 100,
+  health: 100,
+  damage: 10,
+  gold: 0,
+};
+
+let fightLogNum = 0
+let actLogNum = 0
+let potions = 1
+
+const enemies = {
+  test: {
+    name: "Geremy",
+    maxHealth: 25,
+    health: 25,
     damage: 10,
-  };
-  let winner = "NULL";
-  let IntervalId = "NotApplied";
+    baseGold: 5,
+  },
+};
 
-  const enemies = {
-    test: {
-      MaxHealth: 25,
-      health: 25,
-      damage: 10,
-    },
-  };
-
+window.addEventListener("load", function () {
+  //Fight Func
   document.getElementById("FightEnemy").onclick = () => {
     fightEnemy(document.getElementById("cars").value);
+  };
+  //Check Money
+  document.getElementById("CheckGold").onclick = () => {
+    document.getElementById("ActivityStatus").innerText = (`you have ${player.gold} gold! | ${actLogNum}`)
+    actLogNum += 1
+  };
+  //Heal Player
+  document.getElementById("DrinkPotion").onclick = () => {
+    if (potions > 0) {
+      potions -= 1;
+      player.health += player.maxHealth/4;
+      document.getElementById("ActivityStatus").innerText = (`You healed for ${player.maxHealth/4} health :p | ${actLogNum}`)
+      document.getElementById("PlayerStat").innerText = (`${player.health}/${player.maxHealth}`)
+      actLogNum += 1
+      if (player.health > player.maxHealth) {
+        player.health = player.maxHealth;
+      }
+    }
   };
 
   function fightEnemy(enemy) {
@@ -26,11 +54,26 @@ window.addEventListener("load", function () {
     }
   }
 
+  function calculateRandomGold(
+    baseGold,
+    minMultiplier = 0.5,
+    maxMultiplier = 2
+  ) {
+    const randomMultiplier =
+      Math.random() * (maxMultiplier - minMultiplier) + minMultiplier;
+
+    const finalGold = baseGold * randomMultiplier;
+
+    return Math.round(finalGold);
+  }
+
   function DamageCalc(enemy) {
     player.health -= enemies[enemy].damage;
     enemies[enemy].health -= player.damage;
-    console.log(`player health ${player.health}`);
-    console.log(`enemy health ${enemies[enemy].health}`);
+    document.getElementById("FightStatus").innerText = (`You dealt ${player.damage} to ${enemies[enemy].name} but recieved ${enemies[enemy].damage} | ${fightLogNum}`)
+    document.getElementById("PlayerStat").innerText = (`${player.health}/${player.maxHealth}`)
+
+    fightLogNum += 1
     if (player.health <= 0 || enemies[enemy].health <= 0) {
       clearInterval(IntervalId);
       IntervalId = "Not Applied";
@@ -40,7 +83,8 @@ window.addEventListener("load", function () {
         winner = enemy;
       }
       console.log(winner);
-      enemies[enemy].health = enemies[enemy].MaxHealth;
+      player.gold += calculateRandomGold(enemies[enemy].baseGold);
+      enemies[enemy].health = enemies[enemy].maxHealth;
     }
   }
 });
